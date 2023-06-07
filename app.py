476ae3ae498c752d1fb1f63d40a5ff3a2d0638f5
lazarodenis <135831330+lazarodenis@users.noreply.github.com>
2023-06-07 15:32:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 username = st.text_input("Enter Username")
 age =st.number_input("Enter your age")
-if username==username and age >= 18:
+if username==username and age >=18:
   st.write("Vote")
 else:
   st.write("Not vote")
