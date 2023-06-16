008dbc2b9c5284db09a627b8a1264ec144242a5a
lazarodenis <135831330+lazarodenis@users.noreply.github.com>
2023-06-16 09:03:36 +0300

    Update wizzy.py

--- wizzy.py
+++ wizzy.py
@@ -6,7 +6,7 @@ st.title('guess the number game')
 #generate a random number between 1 and 100
 number = random.randint(1,100)
 
-guess = st.number_input("enter a number(between 1 and 100):,min_value=1,max_value=100):
+guess = st.number_input("enter a number(between 1 and 100):",min_value=1,max_value=100)
                         
 if st.button("make a guess!"):
     if guess > number:
