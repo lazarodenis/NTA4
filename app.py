af2f1078e1f325cb9eee437d99a72d25c9df5f89
lazarodenis <135831330+lazarodenis@users.noreply.github.com>
2023-06-16 08:28:06 +0300

    Wizzy.py

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import streamlit as st
-username = st.text_input("Enter Username")
-age =st.number_input("Enter your age")
-if username==username and age >= 18 <= 120:
-  st.write("Vote")
-else:
-  st.write("Not vote")
-if age > 120:
-  st.write("Not Human")
+import random
+
+st.title('guess the number game')
+number = random.randint(1,100)
+guess = st.number_input("enter a number(between 1 and 100):,min_value=1,max_value=100)
+if st.button("make a guess!"):
+                        if guess > number:
+                        st.write("too high! try a smaller number.")
+                        elif guess < number:
+                        st.write("too low! try a larger number.")
+                        else:
+                        st.write("congratulation! you have guess the number correctly.")
+                        
